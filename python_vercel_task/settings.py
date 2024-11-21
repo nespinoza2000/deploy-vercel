421de9bbea3f521582d7bc9d80c76754fdcdf946
nespinoza2000 <nespinozasegovia@gmail.com>
2024-11-20 22:54:15 -0300

    update_again

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
